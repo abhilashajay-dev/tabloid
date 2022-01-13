600ac269fd18ef5656c6c8df6eadaabcf0a0c823
abhilashajay-dev <abhilashajay69@gmail.com>
2022-01-13 11:16:48 +0530

    initial commit

---
+++ tabloid/settings/dev.py
@@ -0,0 +1,18 @@
+from .base import *  # noqa
+
+# SECURITY WARNING: don't run with debug turned on in production!
+DEBUG = True
+
+# SECURITY WARNING: keep the secret key used in production secret!
+SECRET_KEY = 'g21lhshy+27)3=koq4o65h0oty+bmud9+d)ek7b2p7(wzal=np'
+
+ALLOWED_HOSTS = ['*']
+
+EMAIL_BACKEND = 'django.core.mail.backends.console.EmailBackend'
+
+WAGTAIL_CACHE = False
+
+try:
+    from .local_settings import *  # noqa
+except ImportError:
+    pass
